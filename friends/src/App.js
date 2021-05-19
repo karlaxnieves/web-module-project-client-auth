ac38c3695eb01a2aa10073298d4f10942c6f379b
Karla Nieves <karlaxnieves@gmail.com>
2021-05-18 18:07:09 -0700

    created a private router

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import Login from './components/Login';
+import Friends from './components/Friends'
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          {localStorage.getItem("token") && <Link to="/protected">Protected Page</Link>}
+        </li>
+      </ul>
+
+      <Switch>
+        <Route path="/friends" components={Friends} />
+        <Route path="/login" component={Login} />
+      </Switch>
     </div>
+  </Router>
   );
 }
 
